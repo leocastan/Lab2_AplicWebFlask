ee26e21921f1e9b59d45cb8ab0e061495adabe43
lecastro3 <lecastro3@espe.edu.ec>
2022-05-25 21:56:54 -0500

    Avance 2

--- todoapp.py
+++ todoapp.py
@@ -18,6 +18,54 @@ lista_tareas = []
 def home():
     return render_template('index.html', lista_tareas = lista_tareas)
 
+#2. Funcion controlador para agregar lista a tarea de pendientes
+#Definicion de la ruta
+@app.route('/enviar', methods=['POST'])
+#Llamar a enviar
+def enviar():
+    #Funcion condicional para enviar los datos del formulario
+    if request.method == 'POST':
+        tarea_descripcion = request.form['tarea_descripcion']
+        tarea_correo = request.form['tarea_correo']
+        tarea_prioridad = request.form['tarea_prioridad']
+        
+        #Funcion condicional para no registrar en caso de datos vacios
+        if tarea_descripcion == '' or tarea_correo == '':
+            #Mensaje de alerta de campos faltantes
+            messagebox.showwarning("¡Alerta!","Ingrese todos los campos")
+            return redirect(url_for('home'))
+
+        else:
+            #Mensaje de autorizacion de registro
+            resultado = messagebox.askquestion("Registrar", "¿Está seguro que desea registrar los datos?")
+            #Funcion condicional de confirmacion de registro
+            if resultado == "yes":
+                lista_tareas.append({'tarea_descripcion': tarea_descripcion, 'tarea_correo': tarea_correo, 'tarea_prioridad': tarea_prioridad })
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('home'))
+
+#3. Funcion controlador para borrar la lista de tareas
+@app.route('/borrar', methods=['POST'])
+def borrar():
+    if request.method == 'POST': 
+        #Funcion condicional para mostrar alerta en caso de no existir     
+        if lista_tareas == []:
+            messagebox.showwarning("¡Alerta!","No existen tareas pendientes")
+            return redirect(url_for('home'))
+
+        else:
+            #Mensaje de autorizacion de borrado
+            resultado = messagebox.askquestion("Borrar datos", "¿Está seguro que desea borrar los datos?")
+            #Funcion condicional de confirmacion de borrado
+            if resultado == "yes":
+                messagebox.showinfo("Info","Los datos han sido borrados")
+                lista_tareas.clear()
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('home'))
+
+                
 #####
 #####
 
